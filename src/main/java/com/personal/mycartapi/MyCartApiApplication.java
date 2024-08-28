e53a04a7fefb20877748027c3707642f834a36ba
Aman Varshney <amanvarshney212@gmail.com>
2024-08-29 00:10:47 +0530

    Started Project....

---
+++ src/main/java/com/personal/mycartapi/MyCartApiApplication.java
@@ -0,0 +1,13 @@
+package com.personal.mycartapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCartApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCartApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/personal/mycartapi/MyCartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.mycartapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCartApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
