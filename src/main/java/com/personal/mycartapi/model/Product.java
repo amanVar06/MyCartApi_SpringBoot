4585fad18e9f040688fac6ce767becb5e12f813d
Aman Varshney <amanvarshney212@gmail.com>
2024-08-29 11:31:30 +0530

    Using Response Entity to send response and implemented getProductById.

--- src/main/java/com/personal/mycartapi/model/Product.java
+++ src/main/java/com/personal/mycartapi/model/Product.java
@@ -1,5 +1,6 @@
 package com.personal.mycartapi.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,6 +26,8 @@ public class Product {
     private int quantity;
     private String brand;
     private String category;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date releaseDate;
     private boolean isAvailable;
 
